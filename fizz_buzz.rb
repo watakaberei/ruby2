83ee80c0218e4a0417c6f1fff9803e59b554031f
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-11-06 01:06:38 +0000

    練習問題

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,16 +1,16 @@
 def fizz_buzz(number)
   if number % 15 == 0
-    "FizzBuzz"
+    "Fizz_Buzz"
   elsif number % 3 == 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
-  else
+  else 
     number.to_i
   end
 end
 
-puts "数字を入力してください。"
+puts "数字を入れてください"
 
 input = gets.to_i
 
